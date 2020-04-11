1ad2c7d28ecee2395f7f075adc1c3e480b2781be
Julie Ng <me@julie.io>
2020-04-11 10:58:00 +0200

    util: remove unused extend()

--- healthcheck/util/extend.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-/* eslint-disable no-prototype-builtins */
-
-module.exports = function (subj, opts, key) {
-	if (opts.hasOwnProperty(key)) {
-		subj[key] = opts[key]
-	}
-	return subj
-}
--- healthcheck/util/index.js
+++ healthcheck/util/index.js
@@ -1,6 +1,5 @@
 module.exports = {
 	hasUptimeProps: require('./has-uptime-props'),
 	humanUptime: require('./human-uptime'),
-	isSecret: require('./is-secret'),
-	extend: require('./extend')
+	isSecret: require('./is-secret')
 }
